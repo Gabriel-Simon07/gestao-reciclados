151e875222d81a4ce55c2720038853bbe90ef39a
Gabriel Simon <gabrielsimon775@gmail.com>
2023-01-25 13:21:55 -0300

    [implemented] add getters and setters

--- src/gestao_reciclados/Lata.java
+++ src/gestao_reciclados/Lata.java
@@ -2,4 +2,16 @@ package gestao_reciclados;
 
 public class Lata {
 	double peso;
+
+	public Lata(double peso) {
+		this.peso = peso;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
 }
